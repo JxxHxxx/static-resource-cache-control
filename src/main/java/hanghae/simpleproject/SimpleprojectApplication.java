74f1e780cd219dc85a8cfc95d46817e9a03351b0
JxxHxxx <leesin5498@naver.com>
2022-12-21 15:13:18 +0900

    메인 메소드 출동!

---
+++ src/main/java/hanghae/simpleproject/SimpleprojectApplication.java
@@ -0,0 +1,12 @@
+package hanghae.simpleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleprojectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleprojectApplication.class, args);
+	}
+
+}
